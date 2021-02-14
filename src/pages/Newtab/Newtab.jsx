9dfca8ae8b306a4d3a4863658e0b73acbe60b956
sungryeol <sungryeolp@gmail.com>
2021-02-14 16:19:59 +0900

    :sparkles: added settings.html tab opening feature

--- src/pages/Newtab/Newtab.jsx
+++ src/pages/Newtab/Newtab.jsx
@@ -3,8 +3,9 @@ import React from 'react';
 // import './Newtab.css';
 // import './Newtab.scss';
 import Document from '@/containers/Document';
-import { Heading, Center, Text } from '@chakra-ui/react';
+import { Heading, Center, Text, Button } from '@chakra-ui/react';
 import { NewtabLayout } from '@/containers/layout';
+import openSettings from '@/lib/openSettings';
 
 const Newtab = () => {
   return (
@@ -17,6 +18,9 @@ const Newtab = () => {
         <Center>
           <Text>Please check these to get focused on your work</Text>
         </Center>
+        <Button onClick={openSettings}>
+          click this button to open settings
+        </Button>
       </NewtabLayout>
     </Document>
   );
